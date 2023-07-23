1618eaa65fa011b90b8d7dd077688077ef77edf8
matheusps <cout.matheusps@gmail.com>
2023-07-23 19:37:29 -0300

    add npm publish requirements

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,11 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts"],
+  format: ["cjs", "esm"],
+  external: ["react"],
   splitting: false,
   sourcemap: true,
   clean: true,
+  dts: true,
+  legacyOutput: true,
 });
